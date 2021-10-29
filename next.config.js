58351f32b97a0e9056d6818d90fcdd3fe8723b2d
dromide <mz.hisia@gmail.com>
2021-10-29 20:39:23 +0900

    error-aws-2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   // reactStrictMode: true,
+  webpack(config) {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === 'object')
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+    rules.forEach((rule) => {
+      rule.use.forEach((moduleLoader) => {
+        if (moduleLoader.loader.includes('resolve-url-loader'))
+          moduleLoader.options.sourceMap = false;
+      });
+    });
+
+    return config;
+  },
   i18n: {
     locales: ['en', 'ko'],
     defaultLocale: 'en',
